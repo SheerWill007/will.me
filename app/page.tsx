b21d0c3e138299a4bb22c64c7fbfda22ca054ed7
SheerWill007 <williambenjaminlaw007@gmail.com>
2026-03-07 13:27:30 +0530

    digi store up
    Replaced 'shitposting' with 'posting' and removed the digital store link.

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
                 </p>
 
                 <p>
-                  You'll find me shitposting on{" "}
+                  You'll find me posting on{" "}
                   <a
                     href="https://x.com/"
                     target="_blank"
@@ -52,13 +52,6 @@ export default function Home() {
                     className="relative text-foreground after:absolute after:left-0 after:bottom-0 after:h-[1px] after:w-0 after:bg-foreground after:transition-all after:duration-300 hover:after:w-full"
                   >
                     X
-                  </a>
-                  , check out my{" "}
-                  <a
-                    href="#"
-                    className="relative text-foreground after:absolute after:left-0 after:bottom-0 after:h-[1px] after:w-0 after:bg-foreground after:transition-all after:duration-300 hover:after:w-full"
-                  >
-                    digital store
                   </a>
                   , or reach me at{" "}
                   <a
